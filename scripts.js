42c6db72b6d550f7346ac64906d591fb352807e2
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-05 18:35:38 +0200

    tried exercise 2  in 5

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$('p').css('color', 'purple');
+$('p').css('font-size, 10');
+$('p').text('Cool');
+//Change the background color of the first box to purple, if the color is purple
+$('.East').text('not cool')
+//if the number is bigger than 100 to whoah, that's a big number.
+//otherwise just a regular number, please.
+//Change the text to Power of DOM of the third if the word is cool, otherwise change the fourth one
+
+
+/*$('.South').text(
+    if (cool===cool) {
+        console.log("Power of DOM")    
+    }
+))*/
