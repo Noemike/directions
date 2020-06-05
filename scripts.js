b14e603a23262cf3297bd6f698c24a7b117724b1
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-05 20:59:29 +0200

    finally started to figure it out

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-$('p').css('color', 'purple');
-$('p').css('font-size, 10');
-$('p').text('Cool');
+//$('p').css({color: 'purple'});
+//$('p').css({font-size: 10});
+//$('p').text('Cool');
 //Change the background color of the first box to purple, if the color is purple
-$('.East').text('not cool')
+
 //if the number is bigger than 100 to whoah, that's a big number.
 //otherwise just a regular number, please.
 //Change the text to Power of DOM of the third if the word is cool, otherwise change the fourth one
@@ -13,3 +13,9 @@ $('.East').text('not cool')
         console.log("Power of DOM")    
     }
 ))*/
+
+$('p').css({color: 'purple'});
+$('p').css({fontsize: 10});
+$('p').text('Cool');
+
+$('.East').text('not cool')
