c1e5ba08a8cd336df03731f7c636c4cafa286058
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-08 17:05:49 +0200

    scripts file updated

--- scripts.js
+++ scripts.js
@@ -1,26 +1,23 @@
+console.log('Hi, Noemi')
+
 let color='purple'
 let number=10
 let word='cool'
 
-/*if (color='purple') {
-    $('North').html('purple')
-}*/
-
-/*$('.North').css('background': 'purple'
-    if (color='purple') {
-    }
-)*/
-
-if (number>100) {
-    console.log ("whoah, that's a big number")
-} else {
-    console.log ("just a regular number, please")
+if (color='purple') {
+    $('.North').css("background-color", "purple")
 }
 
-/*$('.North').text(
-    if (number>100) {
-        console.log ("whoah, that's a big number")
-    } else {
-        console.log ("just a regular number, please")
-    } 
-)*/
+if (number>10) {
+    $('.East').html("whoah, that's a big number")
+} else {
+    $('.East').html("just a regular number, please")
+}
+
+//Change the text to Power of DOM of the third if the word is cool, otherwise change the fourth one
+
+if (word="cool") {
+    $('.South').html("Power of DOM")
+} else {
+    $('.West').html("Power of DOM")
+}
